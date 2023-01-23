f7c00026ebf50ec11a0a7f926d73682e8e7458c0
Nicholas Mischke <nmischkework@proton.me>
2023-01-23 07:56:23 -0600

    cleaned up and updated feedexport.py

--- scrapy_sql/feedexport.py
+++ scrapy_sql/feedexport.py
@@ -1,41 +1,14 @@
 
-from scrapy_sql.tableadapter import SQLAlchemyTableAdapter
+from sqlalchemy import create_engine
 from scrapy_sql.scrapy_session import ScrapySession
 
-from sqlalchemy.orm import sessionmaker
-
 from scrapy.extensions.feedexport import IFeedStorage, build_storage
+from scrapy.exceptions import NotConfigured
 from scrapy.utils.misc import load_object
 
 from twisted.internet import threads
 from urllib.parse import urlparse
 from zope.interface import implementer
-from sqlalchemy.inspection import inspect
-from pprint import pprint
-
-from sqlalchemy import create_engine
-from scrapy.exceptions import NotConfigured
-
-
-def print_dir_obj(obj, tab_count=0):
-    print(f"\n\n{'    '*tab_count}{obj.__class__}")
-    print(f"{'    '*tab_count}{obj}\n\n")
-    for attr in dir(obj):
-        try:
-            obj_attr = getattr(obj, attr)
-            # if hasattr(obj_attr, '__call__'):
-            #     print_dir_obj(obj_attr, tab_count=tab_count+1)
-            #     print()
-            # else:
-            if attr == '__dict__':
-                print(f"{'    '*tab_count}{attr}: ")
-                pprint(obj_attr)
-            else:
-                print(f"{'    '*tab_count}{attr} : {obj_attr}")
-            print()
-        except AttributeError:
-            print(f"{'    '*tab_count}{attr}: AttributeError")
-    input()
 
 
 def _default_commit(scrapy_session):
@@ -56,16 +29,15 @@ class SQLAlchemyFeedStorage:
     @classmethod
     def from_crawler(cls, crawler, uri, *, feed_options=None):
 
-        item_classes = feed_options.get('item_classes')
-        if item_classes is None:
-            raise NotConfigured
-        else:
-            metadata = load_object(item_classes[0]).metadata
+        try:
+            item_classes = feed_options['item_classes']
+        except KeyError:
+            return NotConfigured
 
         return build_storage(
             cls,
             uri,
-            metadata=metadata,
+            metadata=load_object(item_classes[0]).metadata,
             commit=load_object(
                 feed_options.get('sqlalchemy_commit')
                 or crawler.settings.get('SQLALCHEMY_COMMIT')
@@ -76,14 +48,12 @@ class SQLAlchemyFeedStorage:
 
     def __init__(self, uri, *, metadata, commit, feed_options=None):
         self.uri = uri
-
         self.engine = create_engine(self.uri)
 
         # Create database if it doesn't already exist
         metadata.create_all(self.engine)
 
         self.commit = commit
-        self.feed_options = feed_options
 
     def open(self, spider):
         return ScrapySession(self.engine)
