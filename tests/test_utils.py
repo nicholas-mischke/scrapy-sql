fa6cfd4541372981d3d87503400472ff062f5a1a
Nicholas Mischke <nmischkework@proton.me>
2023-03-23 05:16:49 -0500

    Unit test for utils.py written and passed

---
+++ tests/test_utils.py
@@ -0,0 +1,69 @@
+
+import pytest
+
+from sqlalchemy import select, Column, Numeric, Integer, Text, Date
+
+from integration_test.quotes.items.models import (
+    QuotesBase, Author, Tag, Quote, t_quote_tag
+)
+from scrapy_sql.utils import *
+
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        ("    \n\tHello, World!    ", "Hello, World!"),
+        ("text          text", "text text"),
+    ]
+)
+def test_clean_text(input, expected):
+    assert clean_text(input) == expected
+
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        (select(Author.id).where(Author.name == "Fat Albert"), True),
+        ('SELECT author.id FROM author WHERE author.name = "Fat Albert"', False),
+        (True, False),
+        ({}, False)
+    ]
+)
+def test_column_value_is_subquery(input, expected):
+    assert column_value_is_subquery(input) == expected
+
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        (Column(Integer), True),
+        (Column(Numeric), True),
+        (Column(Text), False),
+        (Column(Date), False),
+    ]
+)
+def test_is_scalar_column(input, expected):
+    assert is_scalar_column(input) == expected
+
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        (
+            select(Author.id).where(Author.name == "Fat Albert"),
+            'SELECT author.id FROM author WHERE author.name = "Fat Albert"'
+        ),
+    ]
+)
+def test_subquery_to_string(input, expected):
+    assert subquery_to_string(input) == expected
+
+
+@pytest.mark.skip(reason="wrapper func")
+def test_insert():
+    pass
+
+
+@pytest.mark.skip(reason="wrapper func")
+def test_insert_ignore():
+    pass
